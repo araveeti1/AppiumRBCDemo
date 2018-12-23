b348d6ad53f293a65768523985f61b187e9b5c7d
Demo Scotiobank <venkatestscotioa@gmail.com>
2018-12-23 10:38:07 -0500

    Committed the src

--- appiumrbcdemo/src/main/java/com/appium/demo/rbc/appiumrbcdemo/tests/ShopByCategoryTestCases.java
+++ appiumrbcdemo/src/main/java/com/appium/demo/rbc/appiumrbcdemo/tests/ShopByCategoryTestCases.java
@@ -7,6 +7,8 @@ import io.appium.java_client.android.AndroidDriver;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.xml.DOMConfigurator;
@@ -14,7 +16,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.TestRunner;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.appium.demo.rbc.appiumrbcdemo.pages.HomePage;
@@ -24,15 +29,27 @@ import com.appium.demo.rbc.appiumrbcdemo.pages.SignUpPage;
 public class ShopByCategoryTestCases {
 	
 	private static AndroidDriver driver;
+	
+	public static String outPutDirectory = null;
+	
+	@BeforeTest
+	public void setup(ITestContext ctx) {
+		DOMConfigurator.configure("log4j.xml");
+		String baseDir = System.getProperty("user.dir");
+		 Date date= new Date();
+		 outPutDirectory = baseDir+File.separator+"testresults"+File.separator+ new Timestamp(date.getTime());
+	    TestRunner runner = (TestRunner) ctx;
+	    System.out.println("The output Directory is Given By ::" +outPutDirectory);
+	    runner.setOutputDirectory(outPutDirectory);
+	}
 
 	
 	@BeforeClass
 	public void init() {
-		DOMConfigurator.configure("log4j.xml");
+		//DOMConfigurator.configure("log4j.xml");
 		File classpathRoot = new File(System.getProperty("user.dir"));
 		File appDir = new File(classpathRoot, "/Apps/Amazon/");
 		File app = new File(appDir, "Amazon India Online Shopping and Payments_v16.21.0.300_apkpure.com.apk");
-
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
 		capabilities.setCapability("deviceName", "Galaxy");
@@ -65,5 +82,6 @@ public class ShopByCategoryTestCases {
 		
 	}
 
+	
 
 }
